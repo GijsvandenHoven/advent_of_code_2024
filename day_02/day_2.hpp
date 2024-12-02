bc5173fc684e2f15d06fa44077f67e8234fff704
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-02 07:20:38 +0100

    d2 parse

--- day_02/day_2.hpp
+++ day_02/day_2.hpp
@@ -9,12 +9,39 @@
 
 NAMESPACE_DEF(DAY) {
 
+struct Report {
+    explicit Report(const std::string& input) {
+        std::stringstream ss(input);
+        while (! ss.eof()) {
+            int i;
+            ss >> i;
+            data.emplace_back(i);
+        }
+    }
+
+    std::vector<int> data;
+};
+
+inline std::ostream& operator<< (std::ostream& os, const Report& r) {
+    os << "Report  {\n\t";
+    for (auto& rr : r.data) {
+        os << rr << ", ";
+    }
+    os << "\n}";
+    return os;
+}
+
 CLASS_DEF(DAY) {
     public:
     DEFAULT_CTOR_DEF(DAY)
 
     void parse(std::ifstream &input) override {
+        std::string line;
+        while (std::getline(input, line)) {
+            reports.emplace_back(line);
+        }
 
+        std::cout << reports.back() << "\n";
     }
 
     void v1() const override {
@@ -30,6 +57,7 @@ CLASS_DEF(DAY) {
     }
 
     private:
+    std::vector<Report> reports;
 };
 
 } // namespace
