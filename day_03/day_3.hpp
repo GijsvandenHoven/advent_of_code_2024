bccf0bc75852ec27336d6a9d975ea82ecf3c9432
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-03 06:58:08 +0100

    prep for pt 2

--- day_03/day_3.hpp
+++ day_03/day_3.hpp
@@ -28,7 +28,10 @@ enum class State : uint8_t {
 struct MachineState {
     int v1 = -1;
     int v2 = -1;
+    bool enabled = true;
 };
+
+typedef std::map<State, std::function<State(char, MachineState&)>> FSM;
 
 CLASS_DEF(DAY) {
     public:
@@ -46,11 +49,11 @@ CLASS_DEF(DAY) {
 
         memory = ss.str();
 
-        state_machine[State::NONE] = [](char c, MachineState& m) -> State { switch (c) { case 'm': return State::M; default: return State::NONE; } };
-        state_machine[State::M] = [](char c, MachineState& m) -> State { switch (c) { case 'u': return State::MU; default: return State::NONE; } };
-        state_machine[State::MU] = [](char c, MachineState& m) -> State { switch (c) { case 'l': return State::MUL; default: return State::NONE; } };
-        state_machine[State::MUL] = [](char c, MachineState& m) -> State { switch (c) { case '(': return State::MUL_PAREN; default: return State::NONE; } };
-        state_machine[State::MUL_PAREN] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::NONE] = [](char c, MachineState& m) -> State { switch (c) { case 'm': return State::M; default: return State::NONE; } };
+        state_machine_p1[State::M] = [](char c, MachineState& m) -> State { switch (c) { case 'u': return State::MU; default: return State::NONE; } };
+        state_machine_p1[State::MU] = [](char c, MachineState& m) -> State { switch (c) { case 'l': return State::MUL; default: return State::NONE; } };
+        state_machine_p1[State::MUL] = [](char c, MachineState& m) -> State { switch (c) { case '(': return State::MUL_PAREN; default: return State::NONE; } };
+        state_machine_p1[State::MUL_PAREN] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v1 = 0;
                 m.v1 += c - '0';
@@ -58,7 +61,7 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_1_1] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::DIGIT_1_1] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v1 *= 10;
                 m.v1 += c - '0';
@@ -68,7 +71,7 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_1_2] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::DIGIT_1_2] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v1 *= 10;
             m.v1 += c - '0';
@@ -78,8 +81,8 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_1_3] = [](char c, MachineState& m) -> State { switch (c) { case ',': return State::COMMA; default: return State::NONE; } };
-        state_machine[State::COMMA] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::DIGIT_1_3] = [](char c, MachineState& m) -> State { switch (c) { case ',': return State::COMMA; default: return State::NONE; } };
+        state_machine_p1[State::COMMA] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v2 = 0;
                 m.v2 += c - '0';
@@ -87,7 +90,7 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_2_1] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::DIGIT_2_1] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v2 *= 10;
                 m.v2 += c - '0';
@@ -97,7 +100,7 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_2_2] = [](char c, MachineState& m) -> State { switch (c) {
+        state_machine_p1[State::DIGIT_2_2] = [](char c, MachineState& m) -> State { switch (c) {
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                 m.v2 *= 10;
                 m.v2 += c - '0';
@@ -107,11 +110,13 @@ CLASS_DEF(DAY) {
             default:
                 return State::NONE;
         } };
-        state_machine[State::DIGIT_2_3] = [](char c, MachineState& m) -> State { switch (c) { case ')': return State::PAREN_CLOSE; default: return State::NONE; } };
-        state_machine[State::PAREN_CLOSE] = [](char c, MachineState& m) -> State { throw std::logic_error("Expected reset"); };
+        state_machine_p1[State::DIGIT_2_3] = [](char c, MachineState& m) -> State { switch (c) { case ')': return State::PAREN_CLOSE; default: return State::NONE; } };
+        state_machine_p1[State::PAREN_CLOSE] = [](char c, MachineState& m) -> State { throw std::logic_error("Expected reset"); };
+
+        state_machine_p2 = state_machine_p1;
     }
 
-    void v1() const override {
+    int solve(const FSM& fsm) const {
         std::stringstream ss (memory);
         MachineState m;
         State s = State::NONE;
@@ -121,9 +126,8 @@ CLASS_DEF(DAY) {
             int c = ss.get();
             if (c == EOF) break;
 
-            //s = state_machine[s](static_cast<char>(c), m);
-            auto iter = state_machine.find(s);
-            if (iter == state_machine.end()) {
+            auto iter = fsm.find(s);
+            if (iter == fsm.end()) {
                 throw std::logic_error("Unknown enum value");
             }
 
@@ -131,18 +135,21 @@ CLASS_DEF(DAY) {
 
             if (s == State::PAREN_CLOSE) {
                 s = State::NONE;
-                sum += m.v1 * m.v2;
-                std::cout << sum << " !\n";
-                m = MachineState();
+                if (m.enabled) { // relevant for p2
+                    sum += m.v1 * m.v2;
+                }
             }
         } while (true);
 
+        return sum;
+    }
 
-        reportSolution(sum);
+    void v1() const override {
+        reportSolution(solve(state_machine_p1));
     }
 
     void v2() const override {
-        reportSolution(0);
+        reportSolution(solve(state_machine_p2));
     }
 
     void parseBenchReset() override {
@@ -151,7 +158,8 @@ CLASS_DEF(DAY) {
 
     private:
     std::string memory;
-    std::map<State, std::function<State(char, MachineState&)>> state_machine;
+    FSM state_machine_p1;
+    FSM state_machine_p2;
 };
 
 } // namespace
