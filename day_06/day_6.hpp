ad814725b719577834e7d7e1a7ba29965bcd43b5
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-06 07:35:35 +0100

    problem 1

--- day_06/day_6.hpp
+++ day_06/day_6.hpp
@@ -9,6 +9,63 @@
 
 NAMESPACE_DEF(DAY) {
 
+enum class Facing {
+    N = 0,
+    S,
+    E,
+    W
+};
+
+struct GridNavigator {
+    std::pair<int, int> pos = {-1,-1};
+    Facing f = Facing::N;
+
+    [[nodiscard]] std::pair<int,int> step() const {
+        auto [x,y] = pos;
+        switch (f) {
+            case Facing::N:
+                --y;
+                break;
+            case Facing::S:
+                ++y;
+                break;
+            case Facing::W:
+                --x;
+                break;
+            case Facing::E:
+                ++x;
+                break;
+            default: throw std::logic_error("Non-Exhaustive enum switch");
+        }
+        return { x, y };
+    }
+
+    void rotateRight() {
+        switch (f) {
+            case Facing::N:
+                f = Facing::E;
+                break;
+            case Facing::S:
+                f = Facing::W;
+                break;
+            case Facing::W:
+                f = Facing::N;
+                break;
+            case Facing::E:
+                f = Facing::S;
+                break;
+            default:
+                throw std::logic_error("Non-Exhaustive enum switch");
+        }
+    }
+};
+
+struct LoopingGridNavigator : public GridNavigator {
+    LoopingGridNavigator() : GridNavigator() {}
+
+    std::set<std::pair<Facing, std::pair<int, int>>> memory;
+};
+
 class Grid : public std::vector<std::vector<char>> {
 public:
     Grid() = default;
@@ -17,16 +74,61 @@ public:
         this->emplace_back(l.begin(), l.end());
         size_t x;
         if ((x = l.find_first_of('^')) != std::string::npos) {
-            guardPos = { x, this->size() - 1 };
+            guard.pos = { x, this->size() - 1 };
             this->back()[x] = '.';
+            guard_start_pos = guard.pos; // needed for problem 2.
         }
     }
 
-    std::pair<int, int> guardPos = {-1,-1};
+    bool guardStep() {
+        const auto suggested = guard.step();
+        auto [x,y] = suggested;
+
+        if (! inBounds(suggested)) {
+            return false;
+        }
+
+        if (this->operator[](y)[x] == '#') { // collision.
+            guard.rotateRight();
+        } else { // collision
+            guard.pos = suggested;
+        }
+
+        return true;
+    }
+
+    void markGuard() {
+        this->operator[](guard.pos.second)[guard.pos.first] = 'X';
+    }
+
+    auto guardStartPos() { return this->guard_start_pos; }
+
+private:
+    [[nodiscard]] bool inBounds (const std::pair<int,int>& p) const {
+        const auto [x,y] = p;
+        if (y < 0 || y > this->size() -1) {
+            return false;
+        }
+
+        if (x < 0 || x > this->operator[](y).size() -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    GridNavigator guard;
+
+    std::pair<int,int> guard_start_pos;
+
+    friend std::ostream& operator<<(std::ostream& os, const Grid& g);
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Grid& g) {
     os << "Grid {\n\t";
+    os << "Guard: { (" << g.guard.pos.first << ", " << g.guard.pos.second << ") F: " << (
+        g.guard.f == Facing::N ? 'N' : g.guard.f == Facing::S ? 'S' : g.guard.f == Facing::E ? 'E' : g.guard.f == Facing::W ? 'W' : '?'
+    ) << " }\n\t";
     for (auto& l : g) {
         for (auto c : l) {
             os << c;
@@ -47,20 +149,43 @@ CLASS_DEF(DAY) {
         while (std::getline(input, line)) {
             g.addLine(line);
         }
-
-        std::cout << g << "\n";
     }
 
     void v1() const override {
-        reportSolution(0);
+        Grid copy = g; // immutability.
+
+        bool stepSuccess;
+        do {
+            copy.markGuard();
+            stepSuccess = copy.guardStep();
+        } while (stepSuccess);
+
+        int sum = 0;
+        for (auto& l : copy) {
+            for (auto c : l) {
+                sum += c == 'X';
+            }
+        }
+
+        reportSolution(sum);
     }
 
     void v2() const override {
+        Grid copy = g; // immutability.
+
+        bool stepSuccess;
+        do {
+            copy.markGuard();
+            stepSuccess = copy.guardStep();
+        } while (stepSuccess);
+
+        // use the 'X' marks to try placing a new obstacle, then step to try and find loops.
+
         reportSolution(0);
     }
 
     void parseBenchReset() override {
-
+        g.clear();
     }
 
     private:
