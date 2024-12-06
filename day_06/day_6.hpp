67286684d9bcc301dc71935d6540b70bee716774
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-06 06:42:50 +0100

    parse

--- day_06/day_6.hpp
+++ day_06/day_6.hpp
@@ -9,28 +9,63 @@
 
 NAMESPACE_DEF(DAY) {
 
-    CLASS_DEF(DAY) {
-        public:
-        DEFAULT_CTOR_DEF(DAY)
+class Grid : public std::vector<std::vector<char>> {
+public:
+    Grid() = default;
 
-        void parse(std::ifstream &input) override {
+    void addLine(const std::string& l) {
+        this->emplace_back(l.begin(), l.end());
+        size_t x;
+        if ((x = l.find_first_of('^')) != std::string::npos) {
+            guardPos = { x, this->size() - 1 };
+            this->back()[x] = '.';
+        }
+    }
 
+    std::pair<int, int> guardPos = {-1,-1};
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Grid& g) {
+    os << "Grid {\n\t";
+    for (auto& l : g) {
+        for (auto c : l) {
+            os << c;
+        }
+        os << "\n\t";
+    }
+    os << "\n}";
+    return os;
+}
+
+CLASS_DEF(DAY) {
+    public:
+    DEFAULT_CTOR_DEF(DAY)
+
+    void parse(std::ifstream &input) override {
+        std::string line;
+
+        while (std::getline(input, line)) {
+            g.addLine(line);
         }
 
-        void v1() const override {
-            reportSolution(0);
-        }
+        std::cout << g << "\n";
+    }
 
-        void v2() const override {
-            reportSolution(0);
-        }
+    void v1() const override {
+        reportSolution(0);
+    }
 
-        void parseBenchReset() override {
+    void v2() const override {
+        reportSolution(0);
+    }
 
-        }
+    void parseBenchReset() override {
 
-        private:
-    };
+    }
+
+    private:
+    Grid g;
+};
 
 } // namespace
 
