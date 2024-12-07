9865ec0c0185491eee8cf3b9f2409e899ae454ff
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-07 13:31:26 +0100

    problem 1

--- day_07/day_7.hpp
+++ day_07/day_7.hpp
@@ -23,8 +23,8 @@ struct Problem {
         }
     }
 
-    std::vector<int> values;
-    int goal;
+    std::vector<int64_t> values;
+    int64_t goal;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Problem& p) {
@@ -46,12 +46,58 @@ CLASS_DEF(DAY) {
         }
     }
 
-    void v1() const override {
-        reportSolution(0);
+    static int64_t concat(int64_t left, int64_t right) {
+        while (right != 0) {
+            const auto digit = right % 10;
+            right /= 10;
+            left *= 10;
+            left += digit;
+        }
+
+        return left;
     }
 
+    static bool reachable(int64_t goal, int64_t partial, int idx, const std::vector<int64_t>& values, bool problem_2_enabled) {
+        if (partial == goal) return true; // base case 1: reached
+        if (partial > goal) return false; // base case 2: past the goal, cannot go down.
+        if (idx >= values.size()) return false; // base case 3: not reached but out of values
+
+        auto value = values[idx];
+        bool success = false;
+        // try add
+        success = reachable(goal, partial + value, idx + 1, values, problem_2_enabled);
+        if (success) return success;
+        // try mul
+        success = reachable(goal, partial * value, idx + 1, values, problem_2_enabled);
+        if (success) return success;
+
+        // problem 2: concat
+        if (problem_2_enabled) {
+            success = reachable(goal, concat(partial, value), idx + 1, values, problem_2_enabled);
+        }
+
+        return success;
+    }
+
+    void v1() const override {
+        int64_t can = 0;
+        for (auto& p : problems) {
+            if(reachable(p.goal, 0, 0, p.values, false)) {
+                can += p.goal;
+            }
+        }
+        reportSolution(can);
+    }
+
+    // 32277743467738   - too high.
     void v2() const override {
-        reportSolution(0);
+        int64_t can = 0;
+        for (auto& p : problems) {
+            if(reachable(p.goal, 0, 0, p.values, true)) {
+                can += p.goal;
+            }
+        }
+        reportSolution(can);
     }
 
     void parseBenchReset() override {
