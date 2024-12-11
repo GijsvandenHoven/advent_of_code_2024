b0fada6ea22d811beb4549bc41fc6191a67a6eb4
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-11 06:36:34 +0100

    p1

--- day_11/day_11.hpp
+++ day_11/day_11.hpp
@@ -22,18 +22,43 @@ NAMESPACE_DEF(DAY) {
                 ss >> v;
                 stones.emplace_back(v);
             }
+        }
 
-            for (auto& x : stones) {
-                std::cout << x << "\n";
+        void stoneBlink(int nstep, int64_t stone, std::vector<int64_t>& output, int goal) const {
+            if (nstep == goal) {
+                output.emplace_back(stone);
+                return;
             }
+
+            // rule 1: 0 to 1.
+            if (stone == 0) {
+                stoneBlink(nstep+1, 1, output, goal);
+                return;
+            }
+            // rule 2
+            auto sstr = std::to_string(stone);
+            std::cout << "stoi: " << sstr << "\n";
+            if (sstr.size() % 2 == 0) { // yuck
+                stoneBlink(nstep+1, std::stoi(sstr.substr(0, sstr.size() / 2)), output, goal);
+                stoneBlink(nstep+1, std::stoi(sstr.substr(sstr.size() / 2)), output, goal);
+                return;
+            }
+            // rule 3
+            stoneBlink(nstep + 1, stone * 2024, output, goal);
         }
 
         void v1() const override {
-            reportSolution(0);
+            std::vector<int64_t> result;
+            const int goal = 25;
+            for (auto s : stones) {
+                stoneBlink(0, s, result, goal);
+            }
+
+            reportSolution(result.size());
         }
 
         void v2() const override {
-            reportSolution(0);
+
         }
 
         void parseBenchReset() override {
