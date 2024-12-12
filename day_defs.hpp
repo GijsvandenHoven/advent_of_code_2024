e920bd0727ce02e4682bc754655b4610b1c83eb5
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-12 18:35:58 +0100

    add day 13

--- day_defs.hpp
+++ day_defs.hpp
@@ -33,8 +33,8 @@
 #include "day_11/day_11.hpp"
 // PLACEHOLD(12)
 #include "day_12/day_12.hpp"
-PLACEHOLD(13)
-// #include "day_13/day_13.hpp"
+// PLACEHOLD(13)
+#include "day_13/day_13.hpp"
 PLACEHOLD(14)
 // #include "day_14/day_14.hpp"
 PLACEHOLD(15)
