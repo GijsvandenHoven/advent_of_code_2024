ded52625e1d3039de1dbc37764133b693934c3e3
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-12 07:48:31 +0100

    part 2 wip

--- day_12/day_12.hpp
+++ day_12/day_12.hpp
@@ -13,6 +13,77 @@ struct Blob {
     int area = 0;
     int perimeter = 0;
     char type = '?';
+
+    std::set<std::pair<int,int>> pointcloud;
+
+    [[nodiscard]] int get_sides() const { // n-sides is the number of 90 degree angles that we can detect in the point cloud.
+
+        int sides = 0;
+
+        std::cout << " blob " << type << ":\n";
+
+
+        for (auto [y,x] : pointcloud) {
+            std::cout << "\tconsider " << y << ", " << x << "\n";
+            // 90 degree outer angles
+            if ((! pointcloud.contains({y, x-1})) && (! pointcloud.contains({ y-1, x }))) { // 90 degree angle outer top left
+                std::cout << "\t\tOTL\n";
+                sides++;
+            }
+            if ((! pointcloud.contains({y, x+1})) && (! pointcloud.contains({ y-1, x}))) { // 90 degree angle outer top right
+                std::cout << "\t\tOTR\n";
+                sides++;
+            }
+            if ((! pointcloud.contains({y, x-1})) && (! pointcloud.contains({ y+1, x }))) { // 90 degree angle bot top left
+                std::cout << "\t\tOBL\n";
+                sides++;
+            }
+            if ((! pointcloud.contains({y, x+1})) && (! pointcloud.contains({ y+1, x }))) { // 90 degree angle bot top right
+                std::cout << "\t\tOBR\n";
+                sides++;
+            }
+
+            // 90 degree inner angles
+            if (
+                pointcloud.contains({y, x-1}) &&
+                pointcloud.contains({ y-1, x }) &&
+                    (! pointcloud.contains({x-1, y-1}))
+                ) { // inner corner top left
+                std::cout << "\t\tITL\n";
+                ++sides;
+            }
+            if (
+                pointcloud.contains({y, x+1}) &&
+                pointcloud.contains({ y-1, x }) &&
+                    (! pointcloud.contains({x+1, y-1}))
+                ) { // inner corner top right
+                std::cout << "\t\tITR\n";
+                ++sides;
+            }
+            if (
+                pointcloud.contains({y, x-1}) &&
+                pointcloud.contains({ y+1, x }) &&
+                    (! pointcloud.contains({x-1, y+1}))
+                ) { // inner corner bot left
+                std::cout << "\t\tIBL\n";
+                ++sides;
+            }
+            if (
+                pointcloud.contains({y, x+1}) &&
+                pointcloud.contains({ y+1, x }) &&
+                    (! pointcloud.contains({x+1, y+1}))
+                ) { // inner corner bot right
+                std::cout << "\t\tIBR\n";
+                ++sides;
+            }
+
+            std::cout << "\tnow at " << sides << " sides\n";
+        }
+
+        std::cout << "blob " << type << " has " << sides << " sides\n";
+
+        return sides;
+    }
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Blob& b) {
@@ -49,6 +120,7 @@ CLASS_DEF(DAY) {
             std::cout << "\tAdds to area: " << i << ", " << j << "\n";
             out.area += 1;
             processed.emplace(i, j);
+            out.pointcloud.emplace(i, j); // for problem 2!
             // recursion: try all the neighbours.
             make_blob(i + 1, j, processed, out);
             make_blob(i - 1, j, processed, out);
@@ -83,8 +155,27 @@ CLASS_DEF(DAY) {
         reportSolution(price);
     }
 
-    void v2() const override {
-        reportSolution(0);
+    void v2() const override { // counting sides instead. Count the right-angle corners on the blob.
+        std::set<std::pair<int,int>> processed;
+        std::vector<Blob> blobs;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (! processed.contains({i, j})) { // new thing, let's work out this one.
+                    std::cout << " do blob " << grid[i][j] << "\n";
+                    blobs.emplace_back();
+                    blobs.back().type = grid[i][j];
+                    make_blob(i, j, processed, blobs.back());
+                }
+            }
+        }
+
+        size_t price = 0;
+        for (auto& b : blobs) {
+            price += b.area * b.get_sides();
+            std::cout << b << "\n";
+        }
+
+        reportSolution(price);
     }
 
     void parseBenchReset() override {
