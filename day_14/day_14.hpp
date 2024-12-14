d2d3718e7158ab5f61e8868ae12d1b11211b6871
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-14 05:54:55 +0100

    alignment

--- day_14/day_14.hpp
+++ day_14/day_14.hpp
@@ -9,28 +9,30 @@
 
 NAMESPACE_DEF(DAY) {
 
-    CLASS_DEF(DAY) {
-        public:
-        DEFAULT_CTOR_DEF(DAY)
 
-        void parse(std::ifstream &input) override {
 
-        }
+CLASS_DEF(DAY) {
+    public:
+    DEFAULT_CTOR_DEF(DAY)
 
-        void v1() const override {
-            reportSolution(0);
-        }
+    void parse(std::ifstream &input) override {
 
-        void v2() const override {
-            reportSolution(0);
-        }
+    }
 
-        void parseBenchReset() override {
+    void v1() const override {
+        reportSolution(0);
+    }
 
-        }
+    void v2() const override {
+        reportSolution(0);
+    }
 
-        private:
-    };
+    void parseBenchReset() override {
+
+    }
+
+    private:
+};
 
 } // namespace
 
