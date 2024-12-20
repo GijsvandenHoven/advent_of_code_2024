2d5eb401c130328a618a1eacfd5840457989dbd3
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-20 07:45:43 +0100

    part 2

--- day_20/day_20.hpp
+++ day_20/day_20.hpp
@@ -90,7 +90,7 @@ CLASS_DEF(DAY) {
             int value = (grid[y][x].time - v) - 2; // 2 for the time it took to go through the wall.
 
             if (value >= CHEAT_MIN_VALUE) {
-                std::cout << "\tcheat of value " << value << " at " << y << ", " << x << " from " << v << "\n";
+                // std::cout << "\tcheat of value " << value << " at " << y << ", " << x << " from " << v << "\n";
                 value_cheat_count += 1;
             }
         };
@@ -98,7 +98,7 @@ CLASS_DEF(DAY) {
         std::function cheat_first = [&](int y, int x, int v) { // cheat through a wall.
             if (grid[y][x].type != TileType::WALL) return;
 
-            std::cout << "cheat check on wall at " << y << ", " << x << "\n";
+            // std::cout << "cheat check on wall at " << y << ", " << x << "\n";
 
             do_to_neighbours(y, x, v, cheat_second);
         };
@@ -116,7 +116,44 @@ CLASS_DEF(DAY) {
     }
 
     void v2() const override {
-        reportSolution(0);
+        int valuable_cheats = 0;
+        constexpr int cheat_dist = 20;
+        constexpr int min_cheat_value = 100;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j].type != TileType::EMPTY) continue;
+
+                // in a 20-sized square , if manhattan distance <= 20 check the cheat value of that spot.
+                std::set<std::pair<int,int>> used_endpoints;
+                for (int k =  + cheat_dist; k >= -cheat_dist; --k) {
+                    for (int l = cheat_dist; l >= -cheat_dist; --l) {
+                        int manhattan_dist = std::abs(k) + std::abs(l);
+
+                        if (manhattan_dist > cheat_dist) continue;
+
+                        int endY = i + k;
+                        int endX = j + l;
+
+                        if (endY < 0 || endX < 0 || endY >= grid.size() || endX >= grid[endY].size()) continue;
+                        if (grid[endY][endX].type != TileType::EMPTY) continue;
+                        if (used_endpoints.contains({endY, endX})) continue;
+
+                        used_endpoints.emplace(endY, endX);
+
+                        // determine the value of the cheat
+                        int this_cheat_value = (grid[endY][endX].time - grid[i][j].time) - manhattan_dist;
+
+                        //std::cout << i << ", " << j << " has actionable cheat to " << endY << ", " << endX << " valued " << this_cheat_value << "\n";
+
+                        if (this_cheat_value >= min_cheat_value) {
+                            valuable_cheats += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        reportSolution(valuable_cheats);
     }
 
     void parseBenchReset() override {
