9839ada36928d6fd160b6202a57e6368685af1cd
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-12-23 06:39:39 +0100

    p1

--- day_23/day_23.hpp
+++ day_23/day_23.hpp
@@ -9,28 +9,75 @@
 
 NAMESPACE_DEF(DAY) {
 
-    CLASS_DEF(DAY) {
-        public:
-        DEFAULT_CTOR_DEF(DAY)
+struct Computer {
+    std::string id;
+    std::set<std::string> connections;
 
-        void parse(std::ifstream &input) override {
+    [[nodiscard]] int degree() const { return static_cast<int>(connections.size()); }
+};
 
+CLASS_DEF(DAY) {
+    public:
+    DEFAULT_CTOR_DEF(DAY)
+
+    void parse(std::ifstream &input) override {
+        std::string line;
+        while (std::getline(input, line)) {
+            std::string self = line.substr(0, 2);
+            std::string other = line.substr(3, 2);
+
+            computers[self].id = self;
+            computers[other].id = other;
+
+            computers[self].connections.emplace(other);
+            computers[other].connections.emplace(self);
         }
 
-        void v1() const override {
-            reportSolution(0);
+        for (auto& [k, v] : computers) {
+            std::cout << k << ": ";
+            for (auto& c : v.connections) {
+                std::cout << c << ", ";
+            }
+            std::cout << "\n";
         }
+    }
 
-        void v2() const override {
-            reportSolution(0);
+    void get_triplets(const Computer& src, std::set<std::set<std::string>>& known) const {
+        // for each connection of src
+        for (auto& con : src.connections) {
+            // find another one we have in common. That is a triplet.
+            auto& other = computers.find(con)->second;
+
+            for (auto& c2 : other.connections) {
+                std::set sorted { src.id, con, c2 };
+                if (src.connections.contains(c2) && ! known.contains(sorted)) {
+                    known.emplace(sorted);
+                }
+            }
         }
+    }
 
-        void parseBenchReset() override {
+    void v1() const override {
+        std::set<std::set<std::string>> lex_sorted_triplets;
+        for (auto& [key, value] : computers) {
+            if (key[0] != 't') continue;
 
+            get_triplets(value, lex_sorted_triplets);
         }
+        reportSolution(lex_sorted_triplets.size());
+    }
 
-        private:
-    };
+    void v2() const override {
+        reportSolution(0);
+    }
+
+    void parseBenchReset() override {
+        computers.clear();
+    }
+
+    private:
+    std::map<std::string, Computer> computers;
+};
 
 } // namespace
 
